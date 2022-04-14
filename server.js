efe796ebd132005b1f8cab1bfde7f916fcd59e59
Eric Spencer <spencer22eric@gmail.com>
2022-04-13 21:41:21 -0400

    adjust PORT variable location

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 const { animals } = require('./data/animals.json');
 
-const PORT = process.env.PORT || 3001;
+
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
